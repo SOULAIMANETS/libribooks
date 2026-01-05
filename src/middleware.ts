65a9a2339b33b1fe2e10f4d15557fd97f9702e5e
Soulaimane <soulaimane.dev@gmail.com>
2026-01-05 12:42:55 +0100

    chore: migrate from @supabase/auth-helpers-nextjs to @supabase/ssr

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,21 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { updateSession } from '@/utils/supabase/middleware';
 
 export async function middleware(req: NextRequest) {
-    const res = NextResponse.next();
-    const supabase = createMiddlewareClient({ req, res });
-
-    const {
-        data: { session },
-    } = await supabase.auth.getSession();
+    const { supabaseResponse, user } = await updateSession(req);
 
     // If user is signed in and current path is /admin/login, redirect to /admin/dashboard
-    if (session && req.nextUrl.pathname === '/admin/login') {
+    if (user && req.nextUrl.pathname === '/admin/login') {
         return NextResponse.redirect(new URL('/admin/dashboard', req.url));
     }
 
     // If user is not signed in and current path is /admin/dashboard/*, redirect to /admin/login
-    if (!session && req.nextUrl.pathname.startsWith('/admin/dashboard')) {
+    if (!user && req.nextUrl.pathname.startsWith('/admin/dashboard')) {
         return NextResponse.redirect(new URL('/admin/login', req.url));
     }
 
-    return res;
+    return supabaseResponse;
 }
 
 export const config = {
